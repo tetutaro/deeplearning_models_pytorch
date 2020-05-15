50ca4883c4266778d7712aea58266a773d01270f
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2020-05-16 02:22:48 +0900

    fix directory name

--- data/synset_words2labels.py
+++ data/synset_words2labels.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-wf = open('sample_images/labels.txt', 'wt')
+wf = open('gradcam_samples/labels.txt', 'wt')
 with open('synset_words.txt', 'rt') as rf:
     line = rf.readline()
     while line:
